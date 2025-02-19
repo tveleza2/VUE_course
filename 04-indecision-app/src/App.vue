f9bc4c6ca836a7a5687a63d045f07b85ea72d98f
Tomas <pabloacosta@Tomas-Macbook.local>
2025-02-18 19:01:17 -0500

    03 finished, 04 project started

---
+++ 04-indecision-app/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+    <h1>Mi primera app</h1>
+    <hr>
+    <MyCounter/>
+</template>
+
+
+<script lang="ts" setup>
+import MyCounter from './components/MyCounter.vue';
+
+    
+
+</script>
+
+
---
+++ 04-indecision-app/src/components/MyCounter.vue
@@ -0,0 +1,23 @@
+<template>
+    <section>
+        <h3>Counter: {{ counter }}</h3>
+        <h3>Square: {{ square }}</h3>
+
+        <div>
+            <button @click="counter--">-1</button>
+            <button @click="counter++">+1</button>
+        </div>
+    </section>
+    
+</template>
+
+
+<script lang="ts" setup>
+    import { computed, ref } from 'vue';
+    // console.log("Hello world");
+    const counter = ref(10);
+    const square = computed(()=>counter.value*counter.value);
+
+</script>
+
+
