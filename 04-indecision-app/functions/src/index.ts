74bc50e4f72a6354ac1089add52f9547e7cd9009
tveleza2 <tveleza2@eafit.edu.co>
2025-03-07 09:28:34 -0500

    Functions upated, useMessage passed as a cloud function that returns the response with the url fetched by the API

--- 04-indecision-app/functions/src/index.ts
+++ 04-indecision-app/functions/src/index.ts
@@ -6,9 +6,8 @@
  *
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
-
 import {onRequest} from "firebase-functions/v2/https";
-import * as logger from "firebase-functions/logger";
+// import * as logger from "firebase-functions/logger";
 
 // Start writing functions
 // https://firebase.google.com/docs/functions/typescript
@@ -17,3 +16,44 @@ import * as logger from "firebase-functions/logger";
 //   logger.info("Hello logs!", {structuredData: true});
 //   response.send("Hello from Firebase!");
 // });
+
+export interface ChatMessage{
+    id:number;
+    message:string;
+    itsMine:boolean;
+    image?:string;
+}
+
+export interface YesNoResponse {
+    answer: string;
+    forced: boolean;
+    image:  string;
+}
+
+
+
+export const useMessage = onRequest(async(request, response) =>{
+    const message=request.params[0];
+    
+
+    const getResponse = async() => {
+        const response = await fetch("https://yesno.wtf/api");
+        const data = await(response.json()) as YesNoResponse;
+        return data;
+    }
+
+    const onMessage = async(text:string)=>{
+        debugger;
+        if(!text.endsWith("?")) return;
+        const herAnswer = await getResponse();
+        const resp:ChatMessage = {
+            id:new Date().getTime(),
+            message:herAnswer.answer,
+            itsMine:false,
+            image: herAnswer.image,
+        };
+        return resp;
+    }
+    const resp = await onMessage(message);
+    response.json(resp)}
+)
